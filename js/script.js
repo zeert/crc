05a55cea4fb495256e56b94fab8cf880d2c62f78
Reinaldo Zurita <reinaldo.c.zurita@gmail.com>
2013-02-06 17:18:31 -0300

    Cambios al formulario de contacto y validacion

--- js/script.js
+++ js/script.js
@@ -9,6 +9,38 @@
 
 })();
 
+$(document).ready(function(){
+ 
+ $('#contact-form').validate(
+ {
+  rules: {
+    nombre: {
+      minlength: 2,
+      required: true
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    asunto: {
+      minlength: 2,
+      required: true
+    },
+    mensaje: {
+      minlength: 2,
+      required: true
+    }
+  },
+  highlight: function(element) {
+    $(element).closest('.control-group').removeClass('success').addClass('error');
+  },
+  success: function(element) {
+    element
+    .text('OK!').addClass('valid')
+    .closest('.control-group').removeClass('error').addClass('success');
+  }
+ });
+}); // end document.ready
 
 // Function to animate the height of carousel in case of slides with different heights
 function carouselInit() {
